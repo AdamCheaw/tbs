67c82747d95e02a84d9de5f33318e7332b8c857b
AdamCheaw <cheaw0723@gmail.com>
2016-11-20 13:31:50 +0800

    Revert "first commit"
    This reverts commit ec80b29d5035457582c45184f681ea809b392054.

--- connFB.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/facebook-sdk-v5/autoload.php';
-$fb = new Facebook\Facebook([
-  'app_id' => '500571860138545',
-  'app_secret' => 'f8a70ba3306835762d052c175d91d9d7',
-  'default_graph_version' => 'v2.5',
-]);
-?>
